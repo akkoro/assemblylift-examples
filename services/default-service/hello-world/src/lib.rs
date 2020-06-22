f5b5dabfc1748e2fd0bfef91dc6df071f8cdddf3
xlem <xlem@akkoro.io>
2020-06-22 19:17:59 -0230

    first commit

---
+++ services/default-service/hello-world/src/lib.rs
@@ -0,0 +1,19 @@
+// Generated with assemblylift-cli 0.1.0
+
+extern crate asml_awslambda;
+
+use direct_executor;
+use asml_core::GuestCore;
+use asml_awslambda::{*, AwsLambdaClient, LambdaContext};
+
+use asml_awslambda_iomod::{structs, database::*};
+
+handler!(context: LambdaContext, async {
+    let event = context.event;
+    AwsLambdaClient::console_log(format!("Read event: {:?}", event));
+
+    let input: structs::ListTablesInput = Default::default();
+    aws_dynamodb_list_tables(input).await;
+
+    AwsLambdaClient::success("OK".to_string());
+});
