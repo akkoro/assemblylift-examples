b98e4f64467e00db364123dd38143ad0d3ddd952
xlem <xlem@akkoro.io>
2020-06-22 23:21:26 -0230

    return response & update asml core

--- services/default-service/hello-world/src/lib.rs
+++ services/default-service/hello-world/src/lib.rs
@@ -13,7 +13,7 @@ handler!(context: LambdaContext, async {
     AwsLambdaClient::console_log(format!("Read event: {:?}", event));
 
     let input: structs::ListTablesInput = Default::default();
-    aws_dynamodb_list_tables(input).await;
+    let response = aws_dynamodb_list_tables(input).await;
 
-    AwsLambdaClient::success("OK".to_string());
+    AwsLambdaClient::success(format!("{:?}", response).to_string());
 });
